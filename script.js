55a5e1dc4e77b1b770caf2e24259c39d049e1f15
Sam <tieoneease@gmail.com>
2021-08-11 12:07:54 -0400

    start of board

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var luisContainer; // create a variable for the DOM node
+function main() {
+  // only runs after DOM loaded, so all DOM is available
+  var board = document.getElementById("board");
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
-  // wait for the web browser to finish loading the content first, or else the element won't exist
-  luisContainer = document.querySelector(".luis-container"); // query the DOM node (class selector)
-  luisContainer.innerText = "Bye Luis!!!";
+  main();
 });
